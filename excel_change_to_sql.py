ebadbc66d04d36f41036e8da47c574b1747dc5d0
slowpocker <scrafty100s@icloud.com>
2021-11-22 03:30:24 +0900

    excel_change_to_sqlの修正

--- excel_change_to_sql.py
+++ excel_change_to_sql.py
@@ -1,6 +1,8 @@
 import glob
 import os
 import openpyxl
+import pathlib
+
 
 #excel_path = '/content/drive/MyDrive/テスト/Book1.xlsx'
 excel_path = ''
@@ -17,9 +19,9 @@ for actSheetName in actBook.sheetnames:
     # 該当シートの最大行と最大列を取得
     maxRow = actSheet.max_row
     maxCol = actSheet.max_column
-    # for 列変数 in シート変数.iter_col(開始行,終了行,開始列,終了列)
+    # for 行変数 in シート変数.iter_rows(開始行,終了行,開始列,終了列)
     for row in actSheet.iter_rows(min_row=2, max_row=maxRow, min_col=1, max_col=maxCol):
-    # １行分のテキスト
+    # １行分のテキストの宣言と初期化
         text = ''
         # for セル変数 in 列変数
         for cell in row:
@@ -28,24 +30,29 @@ for actSheetName in actBook.sheetnames:
             colChart = cell.column
             rowChart = cell.row
 
-            if colChart == 1 & rowChart == 2 :
-                text += (f'(\"{cellData}\", ')
-            elif colChart == 1 & rowChart != 2 :
-                text += (f',(\"{cellData}\", ')
+            if colChart == 1:
+                if rowChart == 2:
+                    text += f'(\"{cellData}\", '
+                else:
+                    text += f',(\"{cellData}\", '
             elif colChart == maxCol:
-                text += (f'\"{cellData}\")  ')
+                text += f'\"{cellData}\")  '
             else:
-                text += (f'\"{cellData}\") , ')
+                text += f'\"{cellData}\") , '
 
-        contents.append(text)            
+        contents.append(text)
+        print(text)
+
 # ブック変数.save(Excelファイルのパス)
     actBook.close
 print(contents)
 
-text_path = '/content/drive/MyDrive/SQL/text.txt'
-#if not os.excel_path.exists(text_path):
+dir_path = '/content/drive/MyDrive/SQL'
+if not os.excel_path.exists(dir_path):
 # ディレクトリが存在しない場合、ディレクトリを作成する
-#    os.makedirs(text_path)
+    os.makedirs(dir_path)
+text_path = pathlib.Path(f'{dir_path}/text.txt')
+# text_path = pathlib.Path(file_path)
 for content in contents:
     with open(text_path, mode='w', encoding='cp932', newline='\n', errors='ignore') as f:
         for content in contents:
