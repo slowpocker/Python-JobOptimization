3cb294519ff5231d4a51986cd6acf4f53f794eeb
slowpocker <scrafty100s@icloud.com>
2021-11-22 14:15:45 +0900

    完成版

--- excel_change_to_sql.py
+++ excel_change_to_sql.py
@@ -5,16 +5,19 @@ import pathlib
 
 
 excel_path = '/content/drive/MyDrive/テスト/Book1.xlsx'
+# excelシートの１行を1行のテキストとして保存する配列
 contents = []
 
+# 列座標を（数値）をアルファベットに変換する関数
 # 数値→アルファベット
 def num2alpha(num):
-    if num<=26:
+    if num <= 26:
         return chr(64+num)
-    elif num%26==0:
+    elif num % 26 == 0:
         return num2alpha(num//26-1)+chr(90)
     else:
-        return num2alpha(num//26)+chr(64+num%26)
+        return num2alpha(num//26)+chr(64+num % 26)
+
 
 # ブックの取得
 actBook = openpyxl.load_workbook(excel_path)
@@ -30,38 +33,40 @@ for actSheetName in actBook.sheetnames:
 
     # for 行変数 in シート変数.iter_rows(開始行,終了行,開始列,終了列)
     for row in actSheet.iter_rows(min_row=2, max_row=maxRow, min_col=1, max_col=maxCol):
-    # １行分のテキストの宣言と初期化
+        # １行分のテキストの宣言と初期化
         text = ''
-        # for セル変数 in 列変数
+        # for セル変数 in 行変数
         for cell in row:
             # セルを取得
             cellData = cell.value
+            # Noneだった場合、空文字に変換する
+            if cellData is None:
+                cellData = ''
+            # 列座標と行座標を取得する（数値）
             colChart = cell.column
             rowChart = cell.row
 
             if colChart == 'A' and rowChart == 2:
-                text += f'(¥"{cellData}¥",'
+                text += f' (\'{cellData}\','
             elif colChart == 'A' and rowChart != 2:
-                text += f',(¥"{cellData}¥",'
+                text += f',(\'{cellData}\','
             elif colChart == num2alpha(maxCol):
-                text += f'¥"{cellData}¥") '
+                text += f'\'{cellData}\') '
             else:
-                text += f'¥"{cellData}¥" ,'
+                text += f'\'{cellData}\' ,'
 
         contents.append(text)
 
-# ブック変数.save(Excelファイルのパス)
     actBook.close
 
 dir_path = '/content/drive/MyDrive/SQL'
 if not os.path.exists(dir_path):
-# ディレクトリが存在しない場合、ディレクトリを作成する
+    # ディレクトリが存在しない場合、ディレクトリを作成する
     os.makedirs(dir_path)
-#ファイルのパスを設定する    
+# ファイルのパスを設定する
 text_path = pathlib.Path(f'{dir_path}/text.txt')
 
-for content in contents:
-    with open(text_path, mode='w', encoding='cp932', newline='¥n', errors='ignore') as f:
-        for content in contents:
-            f.write(content)
+with open(text_path, mode='w', encoding='cp932', newline='\n', errors='ignore') as f:
+    for content in contents:
+        f.write('%s\n' % content)
 f.close()
