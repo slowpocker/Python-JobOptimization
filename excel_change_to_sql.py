7cd8219b36879fb11511918195e0dd0aade64880
slowpocker <scrafty100s@icloud.com>
2021-11-22 13:25:51 +0900

    数値をアルファベットに変換する関数を追加

--- excel_change_to_sql.py
+++ excel_change_to_sql.py
@@ -4,9 +4,17 @@ import openpyxl
 import pathlib
 
 
-#excel_path = '/content/drive/MyDrive/テスト/Book1.xlsx'
-excel_path = ''
+excel_path = '/content/drive/MyDrive/テスト/Book1.xlsx'
 contents = []
+
+# 数値→アルファベット
+def num2alpha(num):
+    if num<=26:
+        return chr(64+num)
+    elif num%26==0:
+        return num2alpha(num//26-1)+chr(90)
+    else:
+        return num2alpha(num//26)+chr(64+num%26)
 
 # ブックの取得
 actBook = openpyxl.load_workbook(excel_path)
@@ -19,6 +27,7 @@ for actSheetName in actBook.sheetnames:
     # 該当シートの最大行と最大列を取得
     maxRow = actSheet.max_row
     maxCol = actSheet.max_column
+
     # for 行変数 in シート変数.iter_rows(開始行,終了行,開始列,終了列)
     for row in actSheet.iter_rows(min_row=2, max_row=maxRow, min_col=1, max_col=maxCol):
     # １行分のテキストの宣言と初期化
@@ -30,31 +39,29 @@ for actSheetName in actBook.sheetnames:
             colChart = cell.column
             rowChart = cell.row
 
-            if colChart == 1:
-                if rowChart == 2:
-                    text += f'(\"{cellData}\", '
-                else:
-                    text += f',(\"{cellData}\", '
-            elif colChart == maxCol:
-                text += f'\"{cellData}\")  '
+            if colChart == 'A' and rowChart == 2:
+                text += f'(¥"{cellData}¥",'
+            elif colChart == 'A' and rowChart != 2:
+                text += f',(¥"{cellData}¥",'
+            elif colChart == num2alpha(maxCol):
+                text += f'¥"{cellData}¥") '
             else:
-                text += f'\"{cellData}\") , '
+                text += f'¥"{cellData}¥" ,'
 
         contents.append(text)
-        print(text)
 
 # ブック変数.save(Excelファイルのパス)
     actBook.close
-print(contents)
 
 dir_path = '/content/drive/MyDrive/SQL'
-if not os.excel_path.exists(dir_path):
+if not os.path.exists(dir_path):
 # ディレクトリが存在しない場合、ディレクトリを作成する
     os.makedirs(dir_path)
+#ファイルのパスを設定する    
 text_path = pathlib.Path(f'{dir_path}/text.txt')
-# text_path = pathlib.Path(file_path)
+
 for content in contents:
-    with open(text_path, mode='w', encoding='cp932', newline='\n', errors='ignore') as f:
+    with open(text_path, mode='w', encoding='cp932', newline='¥n', errors='ignore') as f:
         for content in contents:
             f.write(content)
 f.close()
