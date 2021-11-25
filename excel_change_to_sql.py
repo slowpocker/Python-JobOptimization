d20c50e799d8c8e15a6a32644b01e77d119aeed0
slowpocker <scrafty100s@icloud.com>
2021-11-25 22:35:46 +0900

    エクセルから置換後の文字情報を取得する

--- excel_change_to_sql.py
+++ excel_change_to_sql.py
@@ -5,8 +5,11 @@ import pathlib
 
 
 excel_path = '/content/drive/MyDrive/テスト/Book1.xlsx'
-# excelシートの１行を1行のテキストとして保存する配列
-contents = []
+
+dir_path = '/content/drive/MyDrive/SQL'
+if not os.path.exists(dir_path):
+    # ディレクトリが存在しない場合、ディレクトリを作成する
+    os.makedirs(dir_path)
 
 # 列座標を（数値）をアルファベットに変換する関数
 # 数値→アルファベット
@@ -22,14 +25,20 @@ def num2alpha(num):
 # ブックの取得
 actBook = openpyxl.load_workbook(excel_path)
 
+# lists = []
+
+i = 1
 # シート数分ループ
 for actSheetName in actBook.sheetnames:
-
+    
     # アクティブシートを取得
     actSheet = actBook[actSheetName]
     # 該当シートの最大行と最大列を取得
     maxRow = actSheet.max_row
     maxCol = actSheet.max_column
+
+    # excelシートの１行を1行のテキストとして保存する配列
+    contents = []
 
     # for 行変数 in シート変数.iter_rows(開始行,終了行,開始列,終了列)
     for row in actSheet.iter_rows(min_row=2, max_row=maxRow, min_col=1, max_col=maxCol):
@@ -57,16 +66,17 @@ for actSheetName in actBook.sheetnames:
 
         contents.append(text)
 
-    actBook.close
+    # ファイルのパスを設定する
+    text_path = pathlib.Path(f'{dir_path}/sheet{i}.txt')
 
-dir_path = '/content/drive/MyDrive/SQL'
-if not os.path.exists(dir_path):
-    # ディレクトリが存在しない場合、ディレクトリを作成する
-    os.makedirs(dir_path)
-# ファイルのパスを設定する
-text_path = pathlib.Path(f'{dir_path}/text.txt')
+    with open(text_path, mode='w', encoding='cp932', newline='\n', errors='ignore') as f:
+        for content in contents:
+            f.write('%s\n' % content)
+    f.close()
 
-with open(text_path, mode='w', encoding='cp932', newline='\n', errors='ignore') as f:
-    for content in contents:
-        f.write('%s\n' % content)
-f.close()
+    i += 1
+    # lists.append(contents)
+
+actBook.close
+
+
--- sql_edit.py
+++ sql_edit.py
@@ -1,5 +1,6 @@
 ﻿import os
 import shutil
+import openpyxl
 
 # コピー元ファイルの情報
 ori_workname = ''
@@ -8,12 +9,30 @@ ori_workcode = ''
 ori_id = ''
 ori_path = '/content/drive/MyDrive/***'
 
+##########################################
 # 作成するファイル情報
-lists = [
-        ['***', '***', '***', '***'],
-        ['***', '***', '***', '***'],
-        ['***', '***', '***', '***'],
-]
+lists = []
+# ブックの取得
+excel_path = '/content/drive/MyDrive/テスト/Book1.xlsx'
+actBook = openpyxl.load_workbook(excel_path)
+# 最初のシートを取得
+actSheet = actBook.worksheets[0]
+# 該当シートの最大行と最大列を取得
+maxRow = actSheet.max_row
+maxCol = actSheet.max_column
+# for 行変数 in シート変数.iter_rows(開始行,終了行,開始列,終了列)
+for row in actSheet.iter_rows(min_row=2, max_row=maxRow, min_col=1, max_col=maxCol):
+    # excelシートの１行を1行のテキストとして保存する配列
+    contents = []
+    # for セル変数 in 行変数
+    for cell in row:
+        # セルを取得して''の中に入れる
+        text = f'\'{cell.value}\''
+    contents.append(text)
+
+lists.append(contents)
+actBook.close
+##########################################
 
 for list in lists:
     workname = list[0]
@@ -51,8 +70,16 @@ for list in lists:
         # 書き込み用リストを作成
         contents = []
 
+        # ファイルがxml形式ならutf-8,sql形式ならcp932で読み取り、書き込みをする
+        if after_file.endswith('.xml'):
+            char_code = 'utf-8'
+        elif after_file.endswith('.sql'):
+            char_code = 'cp932'
+        else:
+            char_code = 'utf-8'
+
         # ファイル情報を1行ごとに分割したリストとして取得して、作成するファイル情報に合わせて文字置換、contentsに代入していく
-        with open(dest, mode='r', encoding='cp932', newline='\n', errors='ignore') as f:
+        with open(dest, mode='r', encoding=char_code, newline='\n', errors='ignore') as f:
             data_lines = f.readlines()
             for data_line in data_lines:
                 data_line = data_line.replace(ori_formtitle, formtitle).replace(
@@ -61,7 +88,7 @@ for list in lists:
         f.close()
 
         # contentsに代入された1行ごとのテキストをファイルに書き込んでいく
-        with open(dest, mode='w', encoding='cp932', newline='\n', errors='ignore') as f:
+        with open(dest, mode='w', encoding=char_code, newline='\n', errors='ignore') as f:
             for content in contents:
                 f.write(content)
         f.close()
